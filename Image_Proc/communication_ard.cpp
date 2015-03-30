51cf832602bca1a76e7718a69dc01be5fdc0214b
alevar <varaal01@luther.edu>
2015-03-29 22:32:11 -0500

    Simple program for testing the camera feed

--- Image_Proc/communication_ard.cpp
+++ Image_Proc/communication_ard.cpp
@@ -8,19 +8,23 @@ void setup() {
 void serial_read() {
     if (Serial.parseInt() == 314159265)
     {
-        for (int i = 0; i < 4; i++)
+        for (int i = 0; i < 5; i++)
         {
             float new_val = (float(Serial.parseInt()))/100.0;
             pi_data[i] = new_val;
 
         }
+        pi_data[0] = 314159265.0;
     }
+    Serial.println("read");
 }
 
 void loop() {
 
     serial_read();
 
+    Serial.print("First:   ");
+    Serial.print(pi_data[0]);
     Serial.print("  Horizontal:   ");
     Serial.print(pi_data[1]);
     Serial.print("  Vertical:     ");
