ef70dd1933254bd14528c3b8b1b04787006c9385
alevar <varaal01@luther.edu>
2015-10-10 21:29:03 -0500

    Added Serial communication. #8

--- Image_Proc/CPP/color_detect.cpp
+++ Image_Proc/CPP/color_detect.cpp
@@ -1,5 +1,3 @@
-/* OPEN CV LIBRARIES */
-
 #include "opencv2/core/core.hpp"
 #include "opencv2/flann/miniflann.hpp"
 #include "opencv2/imgproc/imgproc.hpp"
@@ -15,28 +13,29 @@
 #include "opencv2/highgui/highgui_c.h"
 #include "opencv2/imgproc/imgproc_c.h"
 
-/* PID LIBRARY */
+#include "PID.h" // PID LIBRARY
 
-#include "PID.h"
+#include <iostream> // GENERAL PURPOSE LIBRARIES
+#include <cmath> // GENERAL PURPOSE LIBRARIES
 
-/* GENERAL PURPOSE LIBRARIES */
+#include <termios.h>
+#include <fcntl.h>
+#include <sys/ioctl.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <string.h>
+#include <unistd.h> 
+#include <stdio.h>
 
-#include <iostream>
-#include <cmath>
+#include <SerialStream.h>
 
-/* USING THE NAMESPACE FOR OPENCV OBJECTS */
-
-using namespace cv;
-
-/* USING NAMESPACE FOR THE GENERAL PURPOSE OBJECTS */
-
-using namespace std;
+using namespace cv; // USING THE NAMESPACE FOR OPENCV OBJECTS
+using namespace std; // USING NAMESPACE FOR THE GENERAL PURPOSE OBJECTS
+using namespace LibSerial ;
 
 /*================================================
 =============== GENERAL DEFINITIONS ==============
 ================================================*/
-
-
 
 int center_frame[2] = {320,240};    // The (x,y) coordinates of the center of the frame with the resolution 640*480
 
@@ -55,20 +54,15 @@ float Kd_hor = 0.03;               // Derivative constant for the PID calculatio
 float Upper_Limit_hor = 10.0;       // Upper limit for the PID output of the distance correction
 float Lower_Limit_hor = -10.0;     // Lower limit for the PID output of the distance correction
 
-
-
 int lowH = 35;
 int highH = 60;
-
 int lowS = 50; 
 int highS = 255;
-
 int lowV = 50;
 int highV = 255;
 
 int largest_area = 15000;
 int largest_contour_index;
-cv::Rect bounding_rect;
 
 int thresh = 100;
 int max_thresh = 255;
@@ -77,6 +71,13 @@ int max_thresh = 255;
 PID myPID(&PID_input_hor,&PID_output_hor,&Setpoint_hor,Kp_hor,Ki_hor,Kd_hor);
 
 int main(int argc, char* argv[]){
+
+	SerialStream serial_port;
+	serial_port.Open("/dev/ttyACM0");
+
+	serial_port.SetBaudRate( SerialStreamBuf::BAUD_9600 );
+	serial_port.SetVTime(1);
+	serial_port.SetVMin(0);
 
 	myPID.SetLimits(&Upper_Limit_hor,&Lower_Limit_hor);
 
@@ -89,14 +90,6 @@ int main(int argc, char* argv[]){
 
 	double Width = source.set(CV_CAP_PROP_FRAME_WIDTH,size[0]); //get the width of frames of the video
 	double Height = source.set(CV_CAP_PROP_FRAME_HEIGHT,size[1]); //get the height of frames of the video
-
-	/* Setting up the Windows for projections */
-
-	namedWindow("Original",CV_WINDOW_AUTOSIZE);
-	namedWindow("HSV",CV_WINDOW_AUTOSIZE);
-	namedWindow("Thresholding",CV_WINDOW_AUTOSIZE);
-
-	RNG rng(12345);
 
 	while(true){
 		
@@ -118,65 +111,56 @@ int main(int argc, char* argv[]){
 		/* THRESHOLDING */
 
 		Mat imageTHR;
-
 		inRange(imageHSV, Scalar(lowH, lowS, lowV), Scalar(highH, highS, highV), imageTHR);
 
 		/* OPENING EROSION/DILUTION */
 
 		erode(imageTHR, imageTHR, getStructuringElement(MORPH_ELLIPSE, Size(8, 8)));
 		dilate(imageTHR, imageTHR, getStructuringElement(MORPH_ELLIPSE, Size(8, 8)));
-
-
-		/* CLOSING EROSION/DILUTION */
-
 		dilate(imageTHR, imageTHR, getStructuringElement(MORPH_ELLIPSE, Size(8, 8))); 
 		erode(imageTHR, imageTHR, getStructuringElement(MORPH_ELLIPSE, Size(8, 8)));
 
-
 		/* PREPPING THE CONTOURS */
-
 		/* BETTER KEEP THOSE REFERENCES AND POINTERS RIGHT */
 		/* REALLY DOES MESS THINGS UP A LOT OF TIMES */
 
 		vector<vector<Point> > contours;
 		vector<Vec4i> hierarchy;
-
 		Mat canny_output;
 
-		/// Detect edges using canny
-		Canny(imageTHR, canny_output, thresh, thresh*2, 3 );
-		/// Find contours
-		findContours(canny_output, contours, hierarchy, CV_RETR_TREE, CV_CHAIN_APPROX_SIMPLE, Point(0, 0) );
+		Canny(imageTHR, canny_output, thresh, thresh*2, 3 ); // Detect edges using canny
+		findContours(canny_output, contours, hierarchy, CV_RETR_TREE, CV_CHAIN_APPROX_SIMPLE, Point(0, 0) ); // Find contours
 
 		vector<vector<Point>> contours_poly(contours.size());
   		vector<Point2f> center(contours.size());
   		vector<float> radius(contours.size());
 
-
 		for( int i = 0; i < contours.size(); i++ )
 	    {
 	        minEnclosingCircle( Mat (contours[i]), center[i], radius[i] );
+	        cout << int(center[i].x) << "\t" << int(center[i].y) << endl;
+	        serial_port << int(center[i].x) << "\n";
 	    }
 
-	    Scalar color = Scalar(255,255,255);
+	    Scalar color = Scalar(255,255,255); // The color of the drawn contour
 
-		Mat drawing = Mat::zeros( canny_output.size(), CV_8UC3 );
+		Mat drawing = Mat::zeros(canny_output.size(), CV_8UC3 );
 		for( int i = 0; i< contours.size(); i++ )
 		{
 		    circle( drawing, center[i], (int)radius[i], color, 2, 8, 0 );
-
-		    
-			cout << int(center[i].x) << "\t" << int(center[i].y) << endl;
 		}
-		
-		namedWindow( "Contours", CV_WINDOW_AUTOSIZE );
-		imshow( "Contours", drawing );
 
-		/* Displaying all three outputs */
+		String msg = "220\n";
 		
-		imshow("Original", frame);
-		imshow("HSV", imageHSV);
-		imshow("Thresholding", imageTHR);
+		
+		// namedWindow("Original",CV_WINDOW_AUTOSIZE);
+		// namedWindow("HSV",CV_WINDOW_AUTOSIZE);
+		// namedWindow("Thresholding",CV_WINDOW_AUTOSIZE);
+		// namedWindow( "Contours", CV_WINDOW_AUTOSIZE );
+		// imshow( "Contours", drawing );		
+		// imshow("Original", frame);
+		// imshow("HSV", imageHSV);
+		// imshow("Thresholding", imageTHR);
 
 		if (waitKey(30) == 27)
 		{
